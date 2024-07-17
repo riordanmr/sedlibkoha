e4b5b8645d4c7a2e032b91aff088a772cd9c9923
Mark Riordan <riordan@rocketmail.com>
2024-07-17 11:31:11 -0700

    placehold.py: add --num arg, and paginate
    This allows us to place holds on many items with a single run of the program.

--- misc/placehold.py
+++ misc/placehold.py
@@ -18,8 +18,9 @@ username = os.getenv("KOHA_USERNAME")
 password = os.getenv("KOHA_PASSWORD")
 
 class Settings:
-    def __init__(self, loc):
-        self.loc = loc    
+    def __init__(self, loc, num):
+        self.loc = loc
+        self.num = num
 
 def initialize():
     global base_url, username, password
@@ -48,54 +49,65 @@ def place_hold(item, settings):
         print("Hold placed on item", item["biblio_id"], "with barcode", item["external_id"])
         return True
     else:
-        print("status code", response.status_code)
-        response_content = response.content.decode('utf-8')
-        print("Response content:", response_content)
+        if response.status_code != 403:
+            print("status code", response.status_code)
+            response_content = response.content.decode('utf-8')
+            print("Response content:", response_content)
         return False
     
-def find_available_item(settings):
+def find_and_hold_items(settings):
     global base_url, username, password
-    # Note: We only retrieve one page of items. If all of them are unavailable,
-    # we will fail to place a hold. 
-    # ToDo: Add pagination.
-    url = base_url + "/api/v1/items"
+    items_url = base_url + "/api/v1/items"
     headers = {
         "Accept": "application/json"
     }
-    response = requests.get(url, auth=HTTPBasicAuth(username, password), headers=headers)
-    if response.status_code > 100 and response.status_code < 400:
-        items = response.json()
-        for item in items:
-            if item["holds_count"] is None:
-                # Unfortunately, most or all items have holds_count = null. 
-                # This would seem to be a bug in Koha. So we have to try to place
-                # the hold and if it fails, try the next item.
-                print("Found available item:", item["biblio_id"], "with barcode", item["external_id"])
-                if place_hold(item, settings):
-                    return item
-                else:
-                    print("Failed to place hold on item; will try with next.")
-        print("No available items found.")
-    else:
-        print("Failed to get items: status code", response.status_code)
-        print("Response content:", response.content)
-    return None
 
+    num_held = 0
+    page = 0
+    per_page = 20
+    # Loop, requesting pages of items until we find one that is available.
+    while True:
+        page += 1
+        url = items_url + f"?_page={page}&_per_page={per_page}"
+        response = requests.get(url, auth=HTTPBasicAuth(username, password), headers=headers)
+        if response.status_code > 100 and response.status_code < 400:
+            items = response.json()
+            for item in items:
+                if item["holds_count"] is None:
+                    # Unfortunately, most or all items have holds_count = null. 
+                    # This would seem to be a bug in Koha. So we have to try to place
+                    # the hold and if it fails, try the next item.
+                    #print("Found available item:", item["biblio_id"], "with barcode", item["external_id"])
+                    if place_hold(item, settings):
+                        num_held += 1
+                        if num_held >= settings.num:
+                            return num_held
+                    else:
+                        #print("Failed to place hold on item; will try with next.")
+                        pass
+            print("No available items in this page; will request the next page.")
+        else:
+            print("Failed to get items: status code", response.status_code)
+            print("Response content:", response.content)
+    
 def parse_args():
     # Initialize the argument parser
     parser = argparse.ArgumentParser(description='Process command line arguments.')
     # Add the 'loc' command line argument
     parser.add_argument('--loc', type=str, default='local', choices=['local', 'remote'],
                         help='Location setting: "local" or "remote"')
+    parser.add_argument('--num', type=int, default=1,
+                        help='Number of items to place holds on')
     
     # Parse the command line arguments
     args = parser.parse_args()
 
-    return Settings(loc=args.loc)
+    return Settings(loc=args.loc, num=args.num)
 
 def main():
     initialize()
     settings = parse_args()
-    item = find_available_item(settings)
+    num_held = find_and_hold_items(settings)
+    print(f"Holds placed on {num_held} items.")
 
 main()
